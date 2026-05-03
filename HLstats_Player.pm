ff9d478390a0ad2f52a75cde69c09babfa69dcc2
SnipeZilla <cedde@msn.com>
2026-05-03 16:07:01 -0400

    Update HLstats_Player.pm

--- HLstats_Player.pm
+++ HLstats_Player.pm
@@ -341,7 +341,7 @@ sub insertPlayer
             ?
         )
     };
-    my @vals = ($self->{name}, $self->{clan}, $::g_servers{$srv_addr}->{game}, $self->{display_events}, $hideval);
+    my @vals = ($self->{name}, $self->{clan}, $::g_servers{$srv_addr}->{game}, $self->{display_events}, $hideranking);
     my $res = ::exec_cache("player_insert", $query, @vals);
     $self->{playerid} = $res->last_insert_id(undef, undef, undef, undef);
 
